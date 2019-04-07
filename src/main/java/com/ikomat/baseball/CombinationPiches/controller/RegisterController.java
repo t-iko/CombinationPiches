395b50b168664f958de83c629a7b703ce45f0c54
Toshihito.Ikoma <live.piece@gmail.com>
2019-04-07 14:07:34 +0900

    daoのインスタンス名の修正

--- src/main/java/com/ikomat/baseball/CombinationPiches/controller/RegisterController.java
+++ src/main/java/com/ikomat/baseball/CombinationPiches/controller/RegisterController.java
@@ -16,7 +16,7 @@ import java.util.List;
 public class RegisterController {
 
     @Autowired
-    private Dao test_dao;
+    private Dao dao;
 
     @RequestMapping(value="/register/match-data", method = RequestMethod.GET)
     public String getMatchData(@ModelAttribute MatchInfo matchDataForm, Model model){
@@ -26,14 +26,14 @@ public class RegisterController {
     @RequestMapping(value="/register/pitch-data", method = RequestMethod.POST)
     @Transactional
     public String postPitchData(@ModelAttribute MatchInfo matchInfo, Model model){
-        test_dao.saveAndFlush(matchInfo);
+        dao.saveAndFlush(matchInfo);
         return "register/pitch-data";
     }
 
     @RequestMapping(value="/test_connectdb", method = RequestMethod.GET)
     @Transactional
     public String testConnectDb(Model model){
-        List<MatchInfo> matchlist= test_dao.findAll();
+        List<MatchInfo> matchlist= dao.findAll();
         model.addAttribute("matchlist",matchlist);
         return "test_connectdb";
     }
