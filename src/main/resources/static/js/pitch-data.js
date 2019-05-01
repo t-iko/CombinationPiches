92ddb92d7650c8abf1c3d65864ee358a7b8c5cc2
Toshihito.Ikoma <live.piece@gmail.com>
2019-05-01 23:32:16 +0900

    コース登録の描画部分の作成

--- src/main/resources/static/js/pitch-data.js
+++ src/main/resources/static/js/pitch-data.js
@@ -1,3 +1,16 @@
 $(function() {
-    $('img').fadeOut();
+    $('#zone').on('click', function(e) {
+        //画像上でクリックした位置を取得
+        console.log('offset=' + e.offsetX + ',' + e.offsetY);
+        var x=e.offsetX;
+        var y=e.offsetY;
+
+        //位置情報をhtmlに渡す
+        $("#text1").val(x);
+        $("#text2").val(y);
+
+        //位置情報をCSSに渡す
+        $("#zone").find("#ball").css('left',x);
+        $("#zone").find("#ball").css('top',y);
+        })
 });
