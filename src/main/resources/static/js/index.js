c95cb7ed6a7c3e3c89a1b04b9e7dca36bc53015f
Toshihito.Ikoma <live.piece@gmail.com>
2019-05-03 12:53:38 +0900

    index.htmlの変更とheaderの作成

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,11 @@
+
+$(function() {
+     $('.header .header-list a').hover(
+        function(){
+            $(this).css('font-size', '110%');
+        },
+        function(){
+            $(this).css('font-size', '100%');
+        }
+    );
+});
